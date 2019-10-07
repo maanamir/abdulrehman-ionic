aad5662b0eea6e0933e116a4e4bd30a826678df9
Abdul Rehman <maan.amir@hotmail.com>
2019-10-07 06:39:33 +0400

    set up project

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Assignment 1
+
+## About
+### This app simply displays my name,City of birth  and  interest.
+
+
+<img src="Capture.PNG" width="752">
+
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  user={
+        name: 'AbdulRehman',
+          city: 'Sharjah',
+          interests:['Sleeping','Eating',
+        'SleepingAgain']
+
+
+
+
+
+  };
+
+  constructor() {}
+
+}
